460b2580cfcf009c1c7441ec3bfe3ae45494a6d6
Marina Schneider <160941462+EscandioNeider@users.noreply.github.com>
2025-02-04 10:47:32 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# 🧮 CALCULADORA 🔢
+
+Este site foi uma proposta de aula de Front-End + Javascript.
+É uma calculadora bem simples, talvez futuramente eu atualize-a.
+
+## Tecnologias Utilizadas:
+- **HTML** para estruturar a página.
+  
+- **CSS** para o design visual.
+  
+- **JavaScript** para efetuar as contas e limpar os campos.
+
+## 🏷 Tags:
+`#Math`  `#Matemática`  `#Calculator` `#Calculadora`
+
+🔗 Visite meu projeto! (https://escandioneider.github.io/Calculadora/)
---
+++ script.js
@@ -0,0 +1,30 @@
+// Função para Soma
+function Soma(){
+
+    n1=parseFloat(document.getElementById("n1").value)
+    console.log(n1)
+
+    n2=parseFloat(document.getElementById("n2").value)
+    console.log(n2)
+
+    document.getElementById("resultado").value = n1 + n2
+}
+    // Função para subtração
+function Sub(){
+    document.getElementById("resultado").value = n1 -n2
+}
+    // Função para Multiplicação
+function Multi(){
+    document.getElementById("resultado").value = n1 * n2
+}
+    // Função para Divisão
+function Divi(){
+    document.getElementById("resultadoS4").value = n1/n2
+}
+
+    // Função AC
+function LimparCampos() {
+    document.getElementById("n1").value = "";
+    document.getElementById("n2").value = "";
+    document.getElementById("resultado").value = "";
+}
